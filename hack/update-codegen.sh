c45862e1a1846c77803455a401beaa3ddf8d7edc
Ankur Sinha <anksinha@redhat.com>
2026-03-04 15:14:04 +0530

    Cleanup
    Signed-off-by: Ankur Sinha <anksinha@redhat.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -17,10 +17,4 @@ kube::codegen::gen_helpers \
   --boilerplate "${SCRIPT_ROOT}/hack/boilerplate.go.txt" \
   "${SCRIPT_ROOT}/pkg/apis"
 
-# # generate client, listers, informers
-# kube::codegen::gen_client \
-#   --with-watch \
-#   --output-dir "${SCRIPT_ROOT}/pkg/generated" \
-#   --output-pkg "${MODULE}/pkg/generated" \
-#   --boilerplate "${SCRIPT_ROOT}/hack/boilerplate.go.txt" \
-#   "${SCRIPT_ROOT}/pkg/apis"
+
